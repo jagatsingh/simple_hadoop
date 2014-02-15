d86f7dbf5638824c3617576d6fd036e18c8ecde2
jagatsingh <jagatsingh@gmail.com>
2014-02-15 12:43:05 +1100

    Spark script

---
+++ scripts/spark/spark_setup.sh
@@ -0,0 +1,14 @@
+# Source common stuff
+source /vagrant/scripts/common.sh
+source /vagrant/install_config.txt
+
+sudo mkdir ${MY_HOME}/${PROJECT}
+cd ${MY_HOME}/${PROJECT}
+
+sudo tar -xzvf /vagrant/binaries/spark/spark*
+
+export_env_variable SPARK_HOME=${MY_HOME}/${PROJECT}/spark-0.9.0-incubating-bin-hadoop2
+
+#echo "PATH=$PATH:${MY_HOME}/${PROJECT}/spark-0.9.0-incubating-bin-hadoop2/bin" >> ${MY_HOME}/.bashrc
+
+
